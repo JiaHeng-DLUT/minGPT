18a70f030cb9991a7850f80c4c365e73db238749
JiaHeng-DLUT <jiaheng.dlut@gmail.com>
2022-03-06 14:39:43 +0800

    04_max_epoch_100_seed_0

---
+++ mingpt/utils/__init__.py
@@ -0,0 +1,7 @@
+from .misc import set_random_seed
+
+
+__all__ = [
+    # misc.py
+    'set_random_seed',
+]
---
+++ mingpt/utils/misc.py
@@ -0,0 +1,12 @@
+import numpy as np
+import random
+import torch
+
+
+def set_random_seed(seed):
+    """Set random seeds."""
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
