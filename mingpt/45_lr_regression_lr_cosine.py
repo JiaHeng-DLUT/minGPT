c3688420f8df34547a2bbe62451a61cd39918bf5
JiaHeng-DLUT <jiaheng.dlut@gmail.com>
2022-03-12 23:56:46 +0800

    Clean code

--- mingpt/45_lr_regression_lr_cosine.py
+++ mingpt/45_lr_regression_lr_cosine.py
@@ -1,17 +1,11 @@
 """
-Simple training loop; Boilerplate that could apply to any arbitrary neural network,
-so nothing in this file really has anything to do with GPT specifically.
+CosineAnnealingLR(optimizer, T_max=7000, eta_min=1e-7)
 """
-
-import math
 import os
-
-from tqdm import tqdm
-
 import torch
 import torch.optim as optim
 from torch.utils.data.dataloader import DataLoader
-
+from tqdm import tqdm
 from data.fly_dataset import FlyNormDataset
 from eval import Evaluator
 from model import GPT, GPT1Config
@@ -41,35 +35,32 @@ class TrainerConfig:
     batch_size = 32
     num_workers = 4
 
-    # optimization parameters
+    # optimizer
     max_epochs = 100
     learning_rate = 1e-5
     betas = (0.9, 0.95)
     grad_norm_clip = 1.0
-    weight_decay = 0.1 # only applied on matmul weights
-    # learning rate decay params: linear warmup followed by cosine decay to 10% of original
-    lr_decay = False
-    warmup_tokens = 375e6 # these two numbers come from the GPT-3 paper, but may not be good defaults elsewhere
-    final_tokens = 260e9 # (at what point we reach 10% of original LR)
+    weight_decay = 0.1      # only applied on matmul weights
 
-    # checkpoint settings
-    ckpt_dir = f'./experiments/fly/45_lr_regression_lr_cosine'
-    # CUDA_VISIBLE_DEVICES=0 python 45_lr_regression_lr_cosine.py > experiments/fly/log/45_lr_regression_lr_cosine.log
-
+    # evaluation
     evaluator_config = {
-            'num_seeds': 3,
-            'num_subtasks': 2,
-            'num_process': 3,
-            'lr_list': [1e-6, 1e-5, 1e-4, \
-                        1e-3, 1e-2, 1e-1, \
-                        1e0, 1e1, 1e2],
-            'batch_size': 8192,
-            'input_dim': 256,
-            'output_dim': 2,
+        'num_seeds': 3,
+        'num_subtasks': 2,
+        'num_process': 1,
+        'lr_list': [1e-6, 1e-5, 1e-4,
+                    1e-3, 1e-2, 1e-1,
+                    1e0, 1e1, 1e2],
+        'batch_size': 1,
+        'input_dim': 256,
+        'output_dim': 2,
     }
 
+    # checkpoint settings
+    ckpt_dir = f'./experiments/fly/45_lr_regression_lr_cosine_reproduce'
+    # CUDA_VISIBLE_DEVICES=0 python 45_lr_regression_lr_cosine.py > experiments/fly/log/45_lr_regression_lr_cosine_reproduce.log
+
     def __init__(self, **kwargs):
-        for k,v in kwargs.items():
+        for k, v in kwargs.items():
             setattr(self, k, v)
 
 
@@ -79,10 +70,11 @@ class Trainer:
         self.model = model
         self.train_dataset = train_dataset
         self.test_dataset = test_dataset
-        self.config = config
-        
+
         config.evaluator_config['num_samples'] = len(test_dataset) * config.clip_frames
         self.evaluator = Evaluator(config.evaluator_config)
+
+        self.config = config
 
         # take over whatever gpus are on the system
         self.device = 'cpu'
@@ -102,7 +94,6 @@ class Trainer:
         raw_model = model.module if hasattr(self.model, 'module') else model
         optimizer = raw_model.configure_optimizers(config)
         scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=7000, eta_min=1e-7)
-
 
         def run_epoch(split):
             is_train = split == 'train'
@@ -125,11 +116,11 @@ class Trainer:
                 # forward the model
                 with torch.set_grad_enabled(is_train):
                     feat, losses = model(x, pos, y)
+
+                if is_train:
                     loss = 0
                     for k, v in losses.items():
                         loss += v
-
-                if is_train:
                     # backprop and update the parameters
                     model.zero_grad()
                     loss.backward()
@@ -142,10 +133,10 @@ class Trainer:
                         print(f', {k}: {v.item():.5f}', end='')
                     print()
                 else:
-                    feat = feat.view(-1, feat.shape[-1]).cpu()
+                    feat = feat.view(-1, feat.shape[-1])
                     feats.append(feat)
                     label = y.transpose(-1, -2)
-                    label = label.reshape(-1, label.shape[-1]).cpu()
+                    label = label.reshape(-1, label.shape[-1])
                     labels.append(label)
 
             if not is_train:
@@ -153,20 +144,16 @@ class Trainer:
                 labels = torch.cat(labels, dim=0)
                 return (feats, labels)
 
-        # if self.test_dataset is not None:
-        #     self.evaluator.eval(*run_epoch('test'))
-        # best_metric = 0
+        if self.test_dataset is not None:
+            best_metric = self.evaluator.eval(*run_epoch('test'))
         self.save_checkpoint(0)
         for epoch in range(config.max_epochs):
             run_epoch('train')
-            torch.cuda.empty_cache()
-            self.save_checkpoint(epoch + 1)
-            # if self.test_dataset is not None:
-            #     metric = self.evaluator.eval(*run_epoch('test'))
-            #     torch.cuda.empty_cache()
-            #     if metric > best_metric:
-            #         best_metric = metric
-            #         self.save_checkpoint(epoch + 1)
+            if self.test_dataset is not None:
+                metric = self.evaluator.eval(*run_epoch('test'))
+                if metric > best_metric:
+                    best_metric = metric
+                    self.save_checkpoint(epoch + 1)
 
 
 if __name__ == '__main__':
@@ -179,10 +166,10 @@ if __name__ == '__main__':
     # val_loader = DataLoader(val_set, batch_size=2, shuffle=False, num_workers=1, pin_memory=True,)
     # print(len(train_loader), len(val_loader))
 
-    gpt_config = GPT1Config(block_size=config.total_frames, 
-        input_dim=config.input_dim, 
-        output_dim=config.output_dim, 
-        num_tokens=config.clip_frames)
+    gpt_config = GPT1Config(block_size=config.total_frames,
+                            input_dim=config.input_dim,
+                            output_dim=config.output_dim,
+                            num_tokens=config.clip_frames)
     model = GPT(gpt_config)
     print(model)
     # x = torch.randn((2, 4500, 528))
