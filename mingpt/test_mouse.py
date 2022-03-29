fb9fd2fbf2f24e5092528e538808b222daa1138c
JiaHeng-DLUT <jiaheng.dlut@gmail.com>
2022-03-29 11:56:50 +0800

    Test m02_axial_mask_wo_mask

--- mingpt/test_mouse.py
+++ mingpt/test_mouse.py
@@ -13,16 +13,16 @@ import torch
 from torch.utils.data.dataloader import DataLoader
 
 from data.mouse_aug_dataset_2 import mouse_aug_dataset_2
-from model import GPT, GPT1Config
+from model2 import GPT, GPT1Config
 from utils.misc import set_random_seed
 
 class TesterConfig:
     # model
-    input_dim = 3 * 12 * 2
+    input_dim = 12 * 2
     output_dim = 128
     total_frames = 1800
     clip_frames = 50
-    num_animals = 11
+    num_animals = 3
 
     # data
     test_dataset = {
@@ -35,9 +35,9 @@ class TesterConfig:
     num_workers = 4
 
     # checkpoint setting
-    ckpt_path = f'./experiments/fly/m00_baseline/epoch26.pth'
+    ckpt_path = f'./experiments/fly/m02_axial_mask/epoch21.pth'
     feat_path = ckpt_path.replace('.pth', '_submission_wo_mask.npy')
-    # CUDA_VISIBLE_DEVICES=3 python test_fly.py
+    # CUDA_VISIBLE_DEVICES=0 python test_mouse.py
 
     def __init__(self, **kwargs):
         for k,v in kwargs.items():
